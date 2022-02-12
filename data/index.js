1dbc1442aac1b28c5513b883b79161140ef8d620
ganguly2021 <sandeepganguly21@gmail.com>
2022-02-12 19:53:46 +0530

    dummy data setup.

---
+++ data/index.js
@@ -0,0 +1,12 @@
+import restaurantData from './restaurant';
+import categoryData from './category';
+
+const initialCurrentLocation = {
+  streetName: 'Kuching',
+  gps: {
+    latitude: 1.5496614931250685,
+    longitude: 110.36381866919922,
+  },
+};
+
+export {restaurantData, categoryData, initialCurrentLocation};
